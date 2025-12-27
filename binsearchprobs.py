eebb091554b5e877e7717a58f6419cb49ad84799
Pratik Mehta <pm.almighty@gmail.com>
2025-12-27 14:48:37 +0000

    Bin

--- binsearchprobs.py
+++ binsearchprobs.py
@@ -1,11 +1,8 @@
 
-
-   def mybinsearch(a,l=0,r=-1,x):
+import math
+def mybinsearch(a,l,r,x):
         ans=-1
-        if r==-1:
-            r=len(a)-1
-        else:
-            while l<=r:
+        while l<=r:
                 m = l + ((r-l)//2)
                 if a[l]==x:
                     ans=l
@@ -15,6 +12,32 @@
                 else:
                     l=m+1
         return ans
+"""
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = low + (high - low) // 2
+
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+"""
+
+#Rectangles
+
+w,h,n = map(int,input().split())
+u=max(n*w+1,n*h+1)
+l=math.floor(math.sqrt(n*w*h))-1
+b=list( ((r//w)*(r//h) - n) for r in range(max(n*w+1,n*h+1)) ) 
+x= mybinsearch(b,l,u,0)
+print(x)
+      
 
 
 
