72e990eb78ad213f0ec23f1051eb76c7939ab1c0
triangulatedcat <pm.almighty@gmail.com>
2025-12-28 10:36:28 +0000

    Ropes1

---
+++ ropes.py
@@ -0,0 +1,34 @@
+def mybinsearch2(f,l,r,x):
+        ans=-1
+        while l<=r:
+                m = l + ((r-l)//2)
+                #print(l,m,r,n*w*h,l**2)
+                if f(l)==x:
+                    ans=l
+                    break
+                elif f(m) >=  x:
+                    r=m
+                else:
+                    l=m+1
+        return ans
+
+def canwecut(x):
+    if x<0:
+        x = x*(-1)
+    pieces=0
+    for i in range(n):
+        pieces+= a[i]//x
+    if pieces >= k:
+         return 1
+    else:
+         return 0
+a=[]
+ubound=0
+n,k = map(int,input().split())
+for i in range(n):
+    a.append((10**6)*int(input()))
+    if a[i] > ubound:
+         ubound=a[i]
+firstans= mybinsearch2(canwecut,-ubound-1,0,1) * (-1)
+theans= f"{str(firstans//(10**6))}.{str(firstans%(10**6))}"
+print(theans) 
