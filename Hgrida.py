d980662e0e211349069a3d569147b639cbaa7f13
triangulatedcat <pm.almighty@gmail.com>
2025-12-31 13:23:52 +0000

    Hgrid2

--- Hgrida.py
+++ Hgrida.py
@@ -1,45 +1,33 @@
-
-def converttobinary(aa):
-    for i in range(len(aa)):
-        if aa[i]==".":
-            aa[i]=0
-        elif aa[i]=="#":
-            aa[i]=1
-    return aa
-
 h,w = map(int,input().split())
 m=[]
-a=[]        
+       
 
 for i in range(h):
-    a=list(map(str,input().split()))
-    m.append(converttobinary(a))
-dp=m
-dpq=[]
+    m.append(list(input().strip()))
+dp= [[0 for i in range(w)] for j in range(h)]
+
 #dp[k][r]= dp[k-1][r] + dp[k][r-1]
 
 #Base-case- the first row:
 for j in range(w):
     if j==0:
-        dpq.append(1)
-    elif dpq[j-1] == 1 and m[0][j]==0:
-        dpq.append(1)
+        dp[0][0]=1
+    elif m[0][j]==".":
+        dp[0][j]=1
     else:
-        dpq.append(0)
-    dp[0][j]=dpq[j]
-dpv=[]
+        break
+
 #Base-Case- The first column:
-for j in range(h):
-    if j==0:
-        dpv.append(1)
-    elif dpv[j-1] == 1 and m[j][0]==0:
-        dpv.append(1)
+for i in range(1,h):
+    if m[i][0]==".":
+        dp[i][0]= 1
     else:
-        dpv.append(0)
-    dp[j][0] = dpv[j]
+        break
 #Base-case done. Do recursion now.    
 for i in range(1,h):
     for j in range(1,w):
-        dp[i][j]= dp[i-1][j] + dp[i][j-1]
+        if m[i][j] == ".":
+            dp[i][j]= dp[i-1][j] + dp[i][j-1]
 
 print(dp[h-1][w-1] % ((10**9)+7))
+#print(dp)
